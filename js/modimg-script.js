b957f1d46156285aab8add250d689caff07e97e4
kylecooltron <kylejcoulon@gmail.com>
2022-06-25 21:01:33 -0600

    added github links to modal popups

--- js/modimg-script.js
+++ js/modimg-script.js
@@ -1,18 +1,48 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-
+const modal = document.querySelector("#myModal");
+const modalTarget = document.querySelector("#myModalContainer");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var modalImg = document.getElementById("modal-img");
 var captionText = document.getElementById("modal-caption");
+var gitHubLink = document.getElementById("modal-git-link");
+
+
 
 document.addEventListener("click", (e) => {
   const elem = e.target;
-  if (elem.id==="modalImg") {
+  const withinBoundaries = e.composedPath().includes(modalTarget)
+
+  if (modal.style.display == "block" && !withinBoundaries) {
+    modal.style.display = "none";
+  }
+
+  if (elem.classList.contains("modalImg")) {
     modal.style.display = "block";
     modalImg.src = elem.dataset.biggerSrc || elem.src;
-    captionText.innerHTML = elem.alt; 
+    captionText.innerHTML = elem.alt;
+
+    gitHubLink.href = elem.dataset.gitLink || "#";
+    gitHubLink.innerHTML = elem.dataset.gitLink || "";
   }
+
 })
+
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+   //if esc key was not pressed in combination with ctrl or alt or shift
+      const isNotCombinedKey = !(event.ctrlKey || event.altKey || event.shiftKey);
+      if (isNotCombinedKey) {
+        if (modal.style.display == "block"){
+          modal.style.display = "none";
+        }
+      }
+  }
+});
+
+
+
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
